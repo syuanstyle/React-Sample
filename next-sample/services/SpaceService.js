44795091b94990c9f4b97153d01879faad8c365a
syuanstyle <92034038+syuanstyle@users.noreply.github.com>
2022-10-20 21:38:09 +0800

    Add files via upload

--- next-sample/services/SpaceService.js
+++ next-sample/services/SpaceService.js
@@ -1,10 +1,30 @@
+const RET_CD_SUCCESS = 0;
+const RET_CD_FAIL = -1;
+
 export async function getSpaceListData() {
 
-    try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    try {
+        const response = await fetch('http://localhost:3000/api/listRet');
         return await response.json();
-    }catch(error) {
+    } catch (error) {
         return [];
     }
-    
+
+}
+
+export function retDataTrans(dataInput) {
+    let dataObj;
+
+    if (dataInput == null) {
+        console.log("dataInput is null");
+        return [];
+    }
+
+    if (RET_CD_SUCCESS == dataInput.retCd) {
+        dataObj = dataInput.retObject;
+    } else if (RET_CD_FAIL == dataInput.retCd) {
+        console.log("data failed");
+    }
+
+    return dataObj;
 }
